be50cbd8e3f9287183ac1fe05fbbc205609e0575
MC19-Dev <clipicimadalin10@yahoo.com>
2022-07-21 18:33:42 +0300

    Add the Spring MVC Controller.

---
+++ src/main/java/ro/spring/jokesapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package ro.spring.jokesapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ro.spring.jokesapp.services.JokeService;
+
+/**
+ * @author MC19
+ */
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
